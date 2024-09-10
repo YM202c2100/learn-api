ba2dcb13f0090c8684db0bd6d51406e3c97e6716
YM202c2100 <y.m202c2100@gmail.com>
2024-09-10 22:49:48 +0900

    phpとmysqlを使ったバックエンドとの連携を行う練習フォルダを作成
    apiはRoute Handlersを用いて実装予定

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function Home(){
   return(
     <ul className="space-y-8 underline">
+      <li><Link href={"/CoopWithPHP"}>Coop with PHP</Link></li>
       <li><Link href={"/serverAction"}>Server Actions</Link></li>
       <li><Link href={"/ReactHookForm"}>React Hook Form</Link></li>
       <li><Link href={"/RouteHandlers"}>Route Handlers</Link></li>
