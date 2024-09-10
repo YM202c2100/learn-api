5c51bf35791c0bb7ec84328b7992c94b75daa6f7
YM202c2100 <y.m202c2100@gmail.com>
2024-09-10 18:34:59 +0900

    routeHandlersの実装がapiRoutesフォルダで行われていたため修正

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,9 @@ import Link from "next/link";
 export default function Home(){
   return(
     <ul className="space-y-8 underline">
-      <li><Link href={"/apiRoutes"}>API Routes</Link></li>
       <li><Link href={"/serverAction"}>Server Actions</Link></li>
       <li><Link href={"/ReactHookForm"}>React Hook Form</Link></li>
-      <li><Link href={"/routeHandlers"}>Route Handlers</Link></li>
+      <li><Link href={"/RouteHandlers"}>Route Handlers</Link></li>
     </ul>
   )
 }
