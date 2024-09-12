be0a2d5318329299b640798f42cbabeb74028637
YM202c2100 <y.m202c2100@gmail.com>
2024-09-12 17:10:32 +0900

    送信後に入力欄の文字を空にするように変更

--- app/CoopWithPHP/components/Todos/Form/Form.tsx
+++ app/CoopWithPHP/components/Todos/Form/Form.tsx
@@ -1,8 +1,11 @@
-import { Dispatch, FormEvent, useState } from "react"
+import { Dispatch, FormEvent, useRef, useState } from "react"
 import { throwErrorWithStatus } from "@/app/CoopWithPHP/api/helper";
 import type { Todo } from "../Todos";
 
 const Form:React.FC<{setTodos:Dispatch<React.SetStateAction<Todo[]>>}> = ({setTodos})=>{
+  const titleRef = useRef<HTMLInputElement>(null);
+  const bodyRef = useRef<HTMLTextAreaElement>(null);
+
   async function submitHandler(e:FormEvent<HTMLFormElement>){
     e.preventDefault();
     const formData = new FormData(e.currentTarget)
@@ -22,6 +25,10 @@ const Form:React.FC<{setTodos:Dispatch<React.SetStateAction<Todo[]>>}> = ({setTo
       const newTodo = await res.json()
 
       setTodos(todos => [...todos, newTodo])
+      if(titleRef.current && bodyRef.current){
+        titleRef.current.value = "";
+        bodyRef.current.value = "";
+      }
 
     } catch (error) {
       console.error(error)
@@ -33,11 +40,11 @@ const Form:React.FC<{setTodos:Dispatch<React.SetStateAction<Todo[]>>}> = ({setTo
       <form onSubmit={submitHandler} method="post" className="ml-4">
         <div className="mb-4">
           <p>タイトル</p>
-          <input type="text" name="title" required className="border border-black"/>
+          <input type="text" ref={titleRef} name="title" required className="border border-black"/>
         </div>
         <div className="mb-4">
           <p>説明文</p>
-          <textarea name="body" className="border border-black"/>
+          <textarea ref={bodyRef} name="body" className="border border-black"/>
         </div>
         <button type="submit" className="border border-black">送信</button>
       </form>
